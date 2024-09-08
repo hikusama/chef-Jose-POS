1e1bb591d9559bcffb33912a7790a05215bc09c5
Hikusama <leomessi25k@gmail.com>
2024-09-08 10:33:13 +0800

    cashier pannel UI 90%

---
+++ UX/script.js
@@ -0,0 +1,77 @@
+$(document).ready(function () {
+    
+
+
+
+
+
+
+    $(".side_nav li").click(function (e) { 
+        e.preventDefault();
+
+        let clicked_location = $(this).attr("id");
+        console.log(clicked_location);
+
+        
+        window.location.href = clicked_location +".html";
+        
+        
+    });
+    $("#screencontroll").click(function (e) { 
+        e.preventDefault();
+        if (!document.fullscreenElement) {
+            $("#screencontroll img").attr("src", "../image/offscreen.png")
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen();
+            } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+                document.documentElement.mozRequestFullScreen();
+            } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari, Opera
+                document.documentElement.webkitRequestFullscreen();
+            } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+                document.documentElement.msRequestFullscreen();
+            }
+        } else {
+            // Exit fullscreen
+            $("#screencontroll img").attr("src", "../image/fullscreen.png")
+
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.mozCancelFullScreen) { // Firefox
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) { // Chrome, Safari, Opera
+                document.webkitExitFullscreen();
+            } else if (document.msExitFullscreen) { // IE/Edge
+                document.msExitFullscreen();
+            }
+        }
+    });
+    $("#refresh").click(function (e) { 
+        e.preventDefault();
+        location.reload()
+    });
+
+
+
+
+    let rotatedimg = false;
+
+    $("#allcategory_open").click(function (e) { 
+        e.preventDefault();
+        if (rotatedimg == false) {
+            $("#allcategory_open img").css("transform","rotate(90deg)")
+            $("#allcategory_open img").css("transition",".3s")
+            $(".category_nav_inner").addClass("category_nav_new");
+            rotatedimg = true;
+            
+        }else{
+            
+            $("#allcategory_open img").css("transform","rotate(0)")
+            $(".category_nav_inner").removeClass("category_nav_new");
+            rotatedimg = false;
+        }
+    });
+ 
+
+
+
+});
