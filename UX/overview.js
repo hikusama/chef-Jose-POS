7f28ede4e90b7f33029815b99a0be8b023adbf3a
Hikusama <leomessi25k@gmail.com>
2024-11-15 22:39:18 +0800

    ui/ux dashboard changed

--- UX/overview.js
+++ UX/overview.js
@@ -1,56 +1,55 @@
 $(document).ready(function () {
-    const ctx = document.getElementById('myBarChart');
+  const ctx = document.getElementById('myBarChart');
 
-    const type = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+  // const type = ['   1st', '2nd', '3rd', '4rth'];
+  const type = ['   Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec   '];
 
-    var myBarChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: type,
-            datasets: [{
-                label: 'Monthly Income 2024',
-                data: [12000, 10109, 3000, 5500, 20000, 8000, 17540, 10050, 16000, 18000, 22078, 40000],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',  
-                    'rgba(54, 162, 235, 0.2)',  
-                    'rgba(255, 206, 86, 0.2)',  
-                    'rgba(75, 192, 192, 0.2)',  
-                    'rgba(153, 102, 255, 0.2)', 
-                    'rgba(255, 159, 64, 0.2)',  
-                    'rgba(255, 205, 86, 0.2)',  
-                    'rgba(75, 192, 192, 0.2)',  
-                    'rgba(201, 203, 207, 0.2)', 
-                    'rgba(54, 162, 235, 0.2)',  
-                    'rgba(153, 102, 255, 0.2)', 
-                    'rgba(255, 99, 132, 0.2)'   
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',   
-                    'rgba(54, 162, 235, 1)',   
-                    'rgba(255, 206, 86, 1)',   
-                    'rgba(75, 192, 192, 1)',   
-                    'rgba(153, 102, 255, 1)',  
-                    'rgba(255, 159, 64, 1)',   
-                    'rgba(255, 205, 86, 1)',   
-                    'rgba(75, 192, 192, 1)',   
-                    'rgba(201, 203, 207, 1)',  
-                    'rgba(54, 162, 235, 1)',   
-                    'rgba(153, 102, 255, 1)',  
-                    'rgba(255, 99, 132, 1)'    
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
+  var myLineChart = new Chart("myBarChart", {
+    type: "line",
+    outLineColor: "rgba(255, 99, 132, 0.4)",
+    data: {
+      labels: type,
+      datasets: [{
+        label: 'This year',
+        data: [860, 1140, 1060, 1060, 1070, 1110, 1330, 2210, 7830, 2478],
+        borderColor: "rgba(54, 162, 235, .8)",
+        fill: false
+      }, {
+        label: 'Last year',
+        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        borderColor: "rgba(255, 99, 132, 0.4)",
+        fill: false
+      }]
+    },
+    options: {
+      legend: { display: false },
 
+    }
+  });
 
+  var pieChartWk = new Chart("pieChartWk", {
+    type: "pie",
+    data: {
+      labels: ["This week","Last Week"],
+      datasets: [{
+        backgroundColor: ["rgba(54, 162, 235, .8)","rgba(255, 99, 132, 0.4)"],
+        data: [50,70]
+      }]
+    }
+  });
 
+  var pieChartD = new Chart("pieChartD", {
+    type: "pie",
+    data: {
+      labels: ["Today","Yesterday"],
+      datasets: [{
+        backgroundColor: ["rgba(54, 162, 235, .8)","rgba(255, 99, 132, 0.4)"],
+        data: [80,10]
+      }]
+    }
+  });
  
+
+
+
 });
